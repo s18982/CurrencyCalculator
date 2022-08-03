c26918e499ffb042e066eb56d3664054189d7aab
s18982@pjwstk.edu.pl <s18982@pjwstk.edu.pl>
2022-08-03 20:05:48 +0200

    proba odczytu pliku

--- CurrencyCalc/src/Main.java
+++ CurrencyCalc/src/Main.java
@@ -1,5 +1,18 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Main {
     public static void main(String[] args) {
-        
+        try {
+            FileInputStream fileInputStream = new FileInputStream("plik.txt");
+            int bajt =fileInputStream.read();
+            while ( bajt != -1) {
+                System.out.print((char) bajt);
+                bajt=fileInputStream.read();
+            }
+            fileInputStream.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
